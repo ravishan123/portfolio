a000fe7b7da06e368e5072787ba484619233ea5e
Amigo <66440787+ravishan123@users.noreply.github.com>
2024-10-15 01:14:45 +0530

    Update Approach.tsx

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -79,11 +79,11 @@ const Card = ({
   children?: React.ReactNode;
   des: string;
 }) => {
-  const [hovered, setHovered] = React.useState(false);
+  const [hovered, setHovered] = React.useState(true);
   return (
     <div
       onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
+      onMouseLeave={() => setHovered(true)}
       // change h-[30rem] to h-[35rem], add rounded-3xl
       className="border border-black/[0.2] group/canvas-card flex items-center justify-center
        dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative lg:h-[35rem] rounded-3xl "
@@ -114,27 +114,18 @@ const Card = ({
       </AnimatePresence>
 
       <div className="relative z-20 px-10">
-        <div
-          // add this for making it center
-          // absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]
-          className="text-center group-hover/canvas-card:-translate-y-4 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] 
-        group-hover/canvas-card:opacity-0 transition duration-200 min-w-40 mx-auto flex items-center justify-center"
-        >
-          {icon}
-        </div>
         <h2
-          // change text-3xl, add text-center
-          className="dark:text-white text-center text-3xl opacity-0 group-hover/canvas-card:opacity-100
-         relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white 
-         group-hover/canvas-card:-translate-y-2 transition duration-200"
+          className="dark:text-white text-center text-3xl opacity-100
+     relative z-10 text-black mt-4 font-bold text-white
+     transition duration-200"
         >
           {title}
         </h2>
-        {/* add this one for the description */}
+        {/* Show description without hover effect */}
         <p
-          className="text-sm opacity-0 group-hover/canvas-card:opacity-100
-         relative z-10 mt-4 group-hover/canvas-card:text-white text-center
-         group-hover/canvas-card:-translate-y-2 transition duration-200"
+          className="text-sm opacity-100
+     relative z-10 mt-4 text-white text-center
+     transition duration-200"
           style={{ color: "#E4ECFF" }}
         >
           {des}
